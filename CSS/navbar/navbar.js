5629e89f3f21114ae40c444faa801c5016b142a1
malbong <gon3578@gmail.com>
2021-07-14 12:32:29 +0900

    fixed navbar__toggleBtn by JS

--- CSS/navbar/navbar.js
+++ CSS/navbar/navbar.js
@@ -1,8 +1,17 @@
-const toggleBtn = document.querySelector('.navbar__toggleBtn');
-const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
+document.addEventListener("DOMContentLoaded", () => {
+	const toggleBtn = document.querySelector('.navbar__toggleBtn');
+	const menu = document.querySelector('.navbar__menu');
+	const icons = document.querySelector('.navbar__icons');
 
-toggleBtn.addEventListener('click', ()=> {
-	menu.classList.toggle('active');
-	icons.classList.toggle('active');
+	toggleBtn.addEventListener('click', () => {
+		const left = toggleBtn.offsetLeft;
+		const top = toggleBtn.offsetTop;
+		console.log(left, top);
+
+		menu.classList.toggle('active');
+		icons.classList.toggle('active');
+
+		toggleBtn.style.left = `${left}px`;
+		toggleBtn.style.top = `${top}px`;
+	});
 });
--- JS/test.js
+++ JS/test.js
@@ -1,5 +1,30 @@
-const array = ['사과', '배', '귤', '바나나'];
+// localStorage 객체
+// -> 웹 브라우저에 데이터를 저장하는 객체
+// -> 제공 메소드
+// 1) localStorage.getItem(myKey): 키에 대한 값 가져오기
+// 2) localStorage.setItem(myKey, myValue): 키에 대한 값 설정
+// 3) localStorage.removeItem(myKey): 해당 키의 값을 제거
+// 4) localStorage.clear(): 저장된 모든 키를 제거
 
-array.forEach((value, index) => {
-	console.log(`${index}번째의 값: ${value}`);
-})
+document.addEventListener('DOMContentLoaded', () => {
+	const input = document.querySelector('input');
+	const button = document.querySelector('button');
+
+	const savedValue = localStorage.getItem('preValue');
+	// const savedValue = localStorage.preValue;// 위와 같음
+
+	if (savedValue) {
+		input.value = savedValue;
+	}
+
+	input.addEventListener('keyup', (event) => {
+		const value = event.currentTarget.value;
+		localStorage.setItem('preValue', value);
+		// localStorage.preValue = value;// 위와 같음
+	});
+	button.addEventListener('click', (event) => {
+		localStorage.clear();
+		input.value = '';
+
+	});
+});
