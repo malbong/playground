d2af8bbdbcab9dee9d4674ff3e66ea5d26bb566c
malbong <gon3578@gmail.com>
2021-07-17 12:46:40 +0900

    modified CSS originLeft to originRight

--- CSS/navbar/navbar.js
+++ CSS/navbar/navbar.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
 	const toggleBtn = document.querySelector('.navbar__toggleBtn');
 	const menu = document.querySelector('.navbar__menu');
 	const icons = document.querySelector('.navbar__icons');
 
 	toggleBtn.addEventListener('click', () => {
-		const left = toggleBtn.offsetLeft;
-		const top = toggleBtn.offsetTop;
-		console.log(left, top);
+		const originLeftPos = toggleBtn.offsetLeft;
+		const originTopPos = toggleBtn.offsetTop;
+		const originRightPos = document.offsetWidth - originLeftPos;
+
+		toggleBtn.style.right = `${originRightPos}px`;
+		toggleBtn.style.top = `${originTopPos}px`;
 
 		menu.classList.toggle('active');
 		icons.classList.toggle('active');
-
-		toggleBtn.style.left = `${left}px`;
-		toggleBtn.style.top = `${top}px`;
 	});
 });
