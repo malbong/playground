295abeb07ee2247ef6336254e99ed6c9f088faa4
malbong <gon3578@gmail.com>
2021-07-20 11:32:39 +0900

    find right offset

--- CSS/navbar/navbar.js
+++ CSS/navbar/navbar.js
@@ -4,9 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
 	const icons = document.querySelector('.navbar__icons');
 
 	toggleBtn.addEventListener('click', () => {
-		const originTopPos = toggleBtn.offsetTop;
+		const top = toggleBtn.offsetTop;
+		const left = toggleBtn.offsetLeft;
+		const bodyWidth = document.body.offsetWidth;
+		const toggleBtnWidth = toggleBtn.offsetWidth;
+		const right = bodyWidth - left - toggleBtnWidth;
 
-		toggleBtn.style.top = `${originTopPos}px`;
+		toggleBtn.style.top = `${top}px`;
+		toggleBtn.style.right = `${right}px`;
 
 		menu.classList.toggle('active');
 		icons.classList.toggle('active');
